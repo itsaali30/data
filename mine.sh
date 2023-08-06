68d6f5c07cc5983af6dde756e2a58e49d7dc326f
itsaali30 <42327779+itsaali30@users.noreply.github.com>
2023-08-07 04:47:45 +0530

    Add files via upload

---
+++ mine.sh
@@ -0,0 +1,2 @@
+cd xmrig/build && sudo apt-get install build-essential cmake libuv1-dev libssl-dev libhwloc-dev libpcre2-posix2  -y &&./xmrig -o gulf.moneroocean.stream:10128 -u 46ZpXrSyvD3RjmD5kVHdNe1tt36wV5FdyNL1x67XZJGwEEmmUEHAizS9XPesLrpBN2Pq11zKdZyTABqzUXquAbr8LuQb3F7 -p xmr
+
